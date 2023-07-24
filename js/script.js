582acd1353ea675331897b07e109a571c66287b9
ibrohimjon7467 <Ibrohimjon7467@gmail.com>
2023-07-24 21:38:44 +0500

    api

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const api = "https://jsonplaceholder.typicode.com/todos";
+const request = new XMLHttpRequest();
+let data = [];
+request.addEventListener("readystatechange", () => {
+  if ((request.readyState == 4) & (request.status == 200)) {
+    data = JSON.parse(request.responseText);
+    addCard(data);
+  } else if (request.readyState == 4) {
+    console.error("Error");
+  } else {
+    console.log("Loading...");
+  }
+});
+request.open("get", api);
+request.send();
+const ul = document.querySelector("ul");
+let radioValue;
+
+function addCard(data) {
+  let result = [];
+  switch (radioValue) {
+    case "true":
+      result = data.filter((item) => item.completed);
+      break;
+    case "false":
+      result = data.filter((item) => !item.completed);
+      break;
+    default:
+      result = data;
+      break;
+  }
+  result.forEach((todo, index) => {
+    ul.innerHTML += `<li>
+    <h3>ID: ${todo.id}</h3>
+    <p>${todo.title}<p>
+    </li>`;
+  });
+}
+const getValue = function (radio) {
+  ul.innerHTML = "";
+  radioValue = radio.value;
+  addCard(data);
+};
